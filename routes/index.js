f384cbd117701d5398f83ae0db5313fb7d1d25dc
Josiah Morris <josiahbmorris@hotmail.com>
2023-08-14 01:35:10 +0900

    Added functions to submit new documents to MongoDB collection from /new

--- routes/index.js
+++ routes/index.js
@@ -12,16 +12,37 @@ async function main() {
   await mongoose.connect(mongoDB)
 }
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Message Board', messages: messages });
+router.get('/', async function(_, res) {
+  await Message.find({}).then((messages) => {res.render('index', { title: 'Mini Message Board', messages: messages })})
 });
 
-router.get('/new', function(req, res, next) {
+router.get('/new', function(_, res) {
   res.render('form')
-}).post('/new', function(req, res, next) {
-  messages.push({text: req.body.message, user: req.body.name, added: new Date()})
-  res.redirect('/')
+}).post('/new', async function(req, res) {
+  await new Message({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date()
+  }).save().then(() => {res.redirect('/')})
 })
+
+const messageSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: String,
+    required: true,
+  },
+  added: {
+    type: Date,
+    required: true,
+  }
+})
+
+const Message = mongoose.model('Message', messageSchema)
 
 module.exports = router;
